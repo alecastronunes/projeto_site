ec31c3f185e2d407095e363add4ce487c0a05e14
alecastronunes <alexandre.castro92@gmal.com>
2018-08-30 09:31:29 -0300

    este commmit serve para gravar as atualizações da página de emoji

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express = require('require');
-const = require('')
+const express = require('express');
+const votacoes = require('./data/votacao.json');
+const fs = require ('fs');
+
+
 
 const app = express();
 app.set('view engine','ejs');
 
 app.use('/static', express.static('static'));
 
+
+
 app.get('',(req,res)=>{
     res.render('index');
 });
 
+app.post('', (req, res) => {
+    let string = `Votação: ${req.body.emoji} \n Por que: ${req.body.porque}  \n`;
+
+    fs.writeFile('votacao.txt', string, {flag: 'a'}, (err) => {
+        res.render('vontacao');
+    });
+});
+
+app.get('/votacao', (req,res)=>{
+    res.render('votacao', {'votacoes': votacoes});//'votacoes' - variavel | votacoes = local
+});
 
 
 
